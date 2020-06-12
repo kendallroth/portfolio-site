4079018d3f730a148a9c036f29f82c66108d7364
Kendall Roth <kendall@kendallroth.ca>
2020-06-12 17:25:08 -0400

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,57 @@
+<template>
+  <Layout :headerOffset="false" :includeFooter="false">
+    <Hero>
+      <div class="page-not-found">
+        <h2 class="page-not-found__title">404</h2>
+        <div class="page-not-found__subtitle">
+          You just hit a page that doesn't exist...yet!
+        </div>
+        <g-link
+          class="page-not-found__action button is-large is-primary mt-md"
+          to="/"
+        >
+          Go to Home
+        </g-link>
+      </div>
+    </Hero>
+  </Layout>
+</template>
+
+<script>
+// Components
+import Hero from "@components/Hero";
+
+export default {
+  name: "PageNotFound",
+  components: {
+    Hero,
+  },
+  metaInfo: {
+    title: "Page Not Found",
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page-not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 32px;
+  text-align: center;
+}
+
+.page-not-found__title {
+  font-size: 10rem;
+  font-weight: bold;
+  color: #d81b60;
+}
+
+.page-not-found__subtitle {
+  font-size: 1.5rem;
+}
+
+.page-not-found__action {
+  font-size: 1.25rem;
+}
+</style>
