bef32023a4d19984883629f0bdbb6da27367face
Kendall Roth <kendall@kendallroth.ca>
2020-06-15 12:36:20 -0400

    Add basic About Page

---
+++ src/components/aboutPage/AboutCard.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="about-card card">
+    <div class="about-card__header">
+      <i class="about-card__icon material-icons">{{ icon }}</i>
+      <div class="about-card__titles">
+        <div class="about-card__title">
+          {{ title }}
+        </div>
+        <div class="about-card__subtitle">
+          <span class="about-card__subtitle__prefix">
+            I am passionate about&hellip;
+          </span>
+          <span class="about-card__subtitle__caption">
+            {{ caption }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <hr class="about-card__divider" />
+    <div class="about-card__content">
+      <slot name="default" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutCard",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$about-card-padding: 16px;
+
+.about-card {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  transition: box-shadow 0.2s ease;
+
+  @include elevate(3);
+
+  &:hover {
+    @include elevate(4);
+
+    .about-card__icon {
+      color: darken($theme-primary-dark, 5%);
+    }
+  }
+}
+
+.about-card__header {
+  display: flex;
+  align-items: center;
+  padding: $about-card-padding;
+}
+
+.about-card__icon {
+  margin-right: $about-card-padding;
+  color: $theme-primary-dark;
+  font-size: 48px;
+  line-height: 1;
+  transition: color 0.2s ease;
+}
+
+.about-card__title {
+  margin-bottom: 4px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  line-height: 1;
+}
+
+.about-card__subtitle {
+  font-size: 0.9rem;
+
+  .about-card__subtitle__prefix {
+    color: $theme-primary-dark;
+  }
+}
+
+.about-card__divider {
+  width: auto;
+  height: 2px;
+  margin: 0 $about-card-padding;
+  background-color: $theme-secondary;
+}
+
+.about-card__content {
+  padding: $about-card-padding;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,77 @@
+<template>
+  <Layout>
+    <PageSection class="py-lg" size="sm-10 md-8">
+      <div class="about-cards">
+        <AboutCard
+          caption="using my dev skills to help people."
+          icon="face"
+          title="About Me"
+        >
+          Content
+        </AboutCard>
+        <AboutCard
+          caption="continual learning by experimenting and making mistakes."
+          icon="school"
+          title="Education"
+        >
+          Content
+        </AboutCard>
+        <AboutCard
+          caption="using my musical and choral gifts to make a difference."
+          icon="music_note"
+          title="Music"
+        >
+          Content
+        </AboutCard>
+        <AboutCard
+          caption="continually living my life in a way to reflect God the Creator."
+          icon="book"
+          title="Faith"
+        >
+          Content
+        </AboutCard>
+        <AboutCard
+          caption="taking time to relax and enjoy life."
+          icon="event_seat"
+          title="Spare Time"
+        >
+          Content
+        </AboutCard>
+        <AboutCard
+          caption="travelling and experiencing different cultures and traditions."
+          icon="local_airport"
+          title="Travelling"
+        >
+          Content
+        </AboutCard>
+      </div>
+    </PageSection>
+  </Layout>
+</template>
+
+<script>
+// Components
+import AboutCard from "@components/aboutPage/AboutCard";
+
+export default {
+  name: "AboutPage",
+  components: {
+    AboutCard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.about-cards {
+  width: 100%;
+
+  > *:not(:first-child) {
+    margin-top: 32px;
+
+    @include smUp() {
+      width: auto;
+      margin: 32px 16px 0 16px;
+    }
+  }
+}
+</style>
